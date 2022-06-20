183a60c5d1c93d1a2a280cf91a545aa19c27c35f
Sam Machado <animiraballes@gmail.com>
2022-06-20 17:42:42 -0300

    add function playRound

--- gameRockPaperScissors.js
+++ gameRockPaperScissors.js
@@ -1,6 +1,6 @@
 console.log('hola');
 
-function computerPlay(){
+function computerSelect(){
     let random = Math.floor((Math.random()*3)+1);
     
     if(random === 1){
@@ -8,15 +8,16 @@ function computerPlay(){
     }else if(random === 2){
         console.log('Paper');
     }else{
-        console.log('Scissors');
+        console.log('Scissor');
     }
+    
     return random;
 }
-alert(computerPlay());
+alert(computerSelect());
 
 
-function playerSelection(){
-    let opSelect = prompt("choise rock, paper or scissors");
+function playerSelect(){
+    let opSelect = prompt("choise rock, paper or scissor");
     opSelect = opSelect.toLowerCase();
     
     if(opSelect == 'rock'){
@@ -31,6 +32,46 @@ function playerSelection(){
     
     return opSelect;
 }
-alert(playerSelection());
+alert(playerSelect());
+
+function playRound(computerSelect, playerSelect){
+    let comput = computerSelect;
+    let player = playerSelect;
+
+    if(((comput == 1 && player == 'rock')||(comput == 2 && player == 'paper')||(comput == 3 && player == 'scissor'))){
+        console.log('TIE!');
+    //TIE
 
 
+    }else if(comput == 'rock' && player == 'paper'){
+        console.log('you win!');
+    //rock paper
+
+    }else if(comput == 'rock' && player == 'scissor'){
+        console.log('you lose!');
+    //rock scissor
+
+
+    }else if(comput == 'paper' && player == 'rock'){
+    console.log('you win!');
+    //paper rock
+
+    }else if(comput == 'paper' && player == 'scissor'){
+    console.log('you lose!');
+    //paper scissor
+
+
+    }else if(comput == 'scissor' && player == 'rock'){
+    console.log('you win!');
+    //scissor rock
+
+    }else if(comput == 'scissor' && player == 'paper'){
+    console.log('you lose!');
+    //scissor paper
+    }else{
+        console.log('failed if');
+    }
+    return;
+}
+
+console.log(playRound());
